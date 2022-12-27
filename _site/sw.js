1bac5f451d510894aa663c877a4dfd8467f617b1
P5232 <118079652+P5232@users.noreply.github.com>
2022-12-27 15:33:29 -0500

    Ran a bundle command locally

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221227.150839';
+const cacheName = 'chirpy-20221227.153118';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
